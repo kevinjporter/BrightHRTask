b844d792bfebd6f23a5a7cb3bb6b6544ae51bb55
Kevin Porter <kevporter20@yahoo.co.uk>
2024-03-10 18:37:18 +0000

    Added some test cases

---
+++ BrightHR.CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrightHR.CheckoutKata.Tests
+{
+    [TestFixture]
+    internal class CheckoutTests
+    {
+        [Test]
+        public void Test_AddSingleItem_CheckTotal()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Test]
+        public void Test_AddOneOfEachItem_CheckTotal()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Test]
+        public void Test_AddMultipliesOfSomeItems_CheckTotal()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Test]
+        public void Test_AddSingleItemWithItemOffer_CheckTotal()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Test]
+        public void Test_AddMixtureOfItemsWithItemOffers_CheckTotal()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
--- BrightHR.CheckoutKata.Tests/obj/Debug/net8.0/BrightHR.CheckoutKata.Tests.AssemblyInfo.cs
+++ BrightHR.CheckoutKata.Tests/obj/Debug/net8.0/BrightHR.CheckoutKata.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrightHR.CheckoutKata.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab30f3100f3266c57e6e613647486a15029fb47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ac2b2e1474a878aa53f45d9792c6fa4ffc9499")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrightHR.CheckoutKata.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrightHR.CheckoutKata.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
